b54770f1362dfc1cdaa8cb60690906ed63aa5f95
PC <nouracherrad722@gmail.com>
2025-03-17 17:32:32 +0000

    basculer vers mysql

--- src/main/java/ma/enset/hopital/Web/PatientController.java
+++ src/main/java/ma/enset/hopital/Web/PatientController.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repo.PatientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 @Controller
@@ -15,9 +18,15 @@ public class PatientController {
     @Autowired
     private PatientRepo patientRepo;
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientList=patientRepo.findAll();
-        model.addAttribute("listPatients",patientList);
+    public String index(Model model,
+        @RequestParam(name = "page",defaultValue = "0") int p,
+        @RequestParam(name = "size" ,defaultValue = "4") int s,
+        @RequestParam(name = "keyword" ,defaultValue = "") String kw){
+        Page<Patient> pagePatients=patientRepo.findByNameContains(kw, PageRequest.of(p, s));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int [pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",p);
+        model.addAttribute("keyword",kw);
         return "patient";
     }
 }
