58e67276069b2126c0ae5121d1b61819ccc24ceb
PC <nouracherrad722@gmail.com>
2025-03-22 17:16:18 +0000

    Créer une page template Faire la validation des formulaires

--- src/main/java/ma/enset/hopital/Web/PatientController.java
+++ src/main/java/ma/enset/hopital/Web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.enset.hopital.Web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repo.PatientRepo;
@@ -8,7 +9,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -36,5 +40,31 @@ public class PatientController {
             @RequestParam(value = "page",defaultValue = "0") int page){
         patientRepo.deleteById(id);
         return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+   @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";}
+
+    @PostMapping("/save")
+    public String savePatient(Model model, @Valid Patient patient , BindingResult bindingResult,
+    @RequestParam(name = "page",defaultValue = "0") int page,
+                              @RequestParam(name = "keyword" ,defaultValue = "") String keyword){
+        if (bindingResult.hasErrors()) return "formPatients";
+        patientRepo.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword; // Redirige vers la liste des patients après la sauvegarde
+    }
+    @GetMapping("/editPatient")
+    public String editPatient(Model model,  Long id , String keyword,int page){
+        Patient patient=patientRepo.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("Patient introuvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("keyword",keyword);
+        model.addAttribute("page",page);
+        return "editPatient"; // Redirige vers la liste des patients après la sauvegarde
     }
 }
--- src/main/java/ma/enset/hopital/entities/Patient.java
+++ src/main/java/ma/enset/hopital/entities/Patient.java
@@ -4,6 +4,9 @@ package ma.enset.hopital.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +23,12 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min=4 ,max=40)
     private String name;
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("1")
     private int score;
 
 
