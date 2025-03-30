13f9050e832b785442976248e645cc9c4a0d48b1
PC <nouracherrad722@gmail.com>
2025-03-30 14:51:25 +0000

    Partie 3 : Sécurité avec Spring security  :    1 - InMemomy Authentication:

--- src/main/java/ma/enset/hopital/Web/PatientController.java
+++ src/main/java/ma/enset/hopital/Web/PatientController.java
@@ -7,6 +7,7 @@ import ma.enset.hopital.repo.PatientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,7 +22,7 @@ import java.util.List;
 public class PatientController {
     @Autowired
     private PatientRepo patientRepo;
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model,
         @RequestParam(name = "page",defaultValue = "0") int p,
         @RequestParam(name = "size" ,defaultValue = "4") int s,
@@ -33,32 +34,36 @@ public class PatientController {
         model.addAttribute("keyword",kw);
         return "patient";
     }
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
+    @PreAuthorize("hasRole('ADMIN')")
     public  String delete(
             @RequestParam("id")  long id,
             @RequestParam(value = "keyword" ,defaultValue = "")  String keyword,
             @RequestParam(value = "page",defaultValue = "0") int page){
         patientRepo.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
     @GetMapping("/")
     public String home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
-   @GetMapping("/formPatients")
+   @GetMapping("/admin/formPatients")
     public String formPatients(Model model){
+
         model.addAttribute("patient",new Patient());
         return "formPatients";}
 
-    @PostMapping("/save")
+    @PostMapping("/admin/save")
+    @PreAuthorize("hasRole('ADMIN')")
     public String savePatient(Model model, @Valid Patient patient , BindingResult bindingResult,
     @RequestParam(name = "page",defaultValue = "0") int page,
                               @RequestParam(name = "keyword" ,defaultValue = "") String keyword){
         if (bindingResult.hasErrors()) return "formPatients";
         patientRepo.save(patient);
-        return "redirect:/index?page="+page+"&keyword="+keyword; // Redirige vers la liste des patients après la sauvegarde
+        return "redirect:/user/index?page="+page+"&keyword="+keyword; // Redirige vers la liste des patients après la sauvegarde
     }
-    @GetMapping("/editPatient")
+    @GetMapping("/admin/editPatient")
+    @PreAuthorize("hasRole('ADMIN')")
     public String editPatient(Model model,  Long id , String keyword,int page){
         Patient patient=patientRepo.findById(id).orElse(null);
         if(patient==null) throw new RuntimeException("Patient introuvable");
@@ -67,4 +72,6 @@ public class PatientController {
         model.addAttribute("page",page);
         return "editPatient"; // Redirige vers la liste des patients après la sauvegarde
     }
+
+
 }
